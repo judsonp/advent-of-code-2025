65ea119a7e2f5426272687e1a9ff67423696810a
Judson Powers <judson.powers@gmail.com>
2025-12-06 14:51:03 -0500

    Day 6 part 2

--- day06/main.py
+++ day06/main.py
@@ -1,5 +1,6 @@
+from itertools import groupby
 from pathlib import Path
-from typing import List
+from typing import List, Tuple
 
 from more_itertools import intersperse
 
@@ -16,29 +17,47 @@ def part_one(data: List[List[str]]) -> int:
     return grand_total
 
 
-def part_two(data: List[List[str]]) -> int:
-    return 0
+def part_two(ops: List[str], numbergrid: List[List[str]]) -> int:
+    grand_total = 0
+    transposed_grid = [list(row) for row in zip(*numbergrid)]
+    raw_numbers_str = ["".join(x) for x in transposed_grid]
+    raw_numbers_int = [int(x) if not x.isspace() else None for x in raw_numbers_str]
+    number_groups = [list(group) for key, group in
+                     groupby(raw_numbers_int, lambda x: x is None) if not key]
+    assert(len(number_groups) == len(ops))
+    for op, numbers in zip(ops, number_groups):
+        problem = list(intersperse(op, [str(x) for x in numbers]))
+        result = eval(" ".join(problem))  # noqa: S307
+        grand_total += result
+    return grand_total
 
 
-def parse(data: str) -> List[List[str]]:
+def parse_one(data: str) -> List[List[str]]:
     return [line.split() for line in data.splitlines()]
+
+
+def parse_two(data: str) -> Tuple[List[str], List[List[str]]]:
+    lines = data.splitlines()
+    ops = lines[-1].split()
+    numbergrid = [list(line) for line in lines[:-1]]
+    return ops, numbergrid
 
 
 def test_part_one() -> None:
     with Path("example.txt").open() as f:
-        data = parse(f.read())
+        data = parse_one(f.read())
     assert part_one(data) == 4277556
 
 
 def test_part_two() -> None:
     with Path("example.txt").open() as f:
-        data = parse(f.read())
-    # assert part_two(data) == 0
+        ops, numbergrid = parse_two(f.read())
+    assert part_two(ops, numbergrid) == 3263827
 
 
 if __name__ == "__main__":
     with Path("input.txt").open() as f:
-        data = parse(f.read())
+        data = f.read()
 
-    print(f"Part 1: {part_one(data)}")
-    print(f"Part 2: {part_two(data)}")
+    print(f"Part 1: {part_one(parse_one(data))}")
+    print(f"Part 2: {part_two(*parse_two(data))}")
